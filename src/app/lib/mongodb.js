7031677dad62b8ea79e53880f45fec1ae7793d39
Zunaira <zunairaabid140@gmail.com>
2026-02-28 12:44:27 +0500

    add layout

--- src/app/lib/mongodb.js
+++ src/app/lib/mongodb.js
@@ -1,44 +1,44 @@
 
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-// ⚠️ Best practice: isay .env.local me rakhein
-const MONGODB_URI = process.env.MONGODB_URI;
+// // ⚠️ Best practice: isay .env.local me rakhein
+// const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-  throw new Error("Please define the MONGODB_URI environment variable");
-}
+// if (!MONGODB_URI) {
+//   throw new Error("Please define the MONGODB_URI environment variable");
+// }
 
-// Global cache (Next.js hot reload ke liye)
-let cached = global.mongoose;
+// // Global cache (Next.js hot reload ke liye)
+// let cached = global.mongoose;
 
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
+// if (!cached) {
+//   cached = global.mongoose = { conn: null, promise: null };
+// }
 
-async function connectDB() {
-  if (cached.conn) return cached.conn;
+// async function connectDB() {
+//   if (cached.conn) return cached.conn;
 
-  const options = {
-    maxPoolSize: 10,
-    serverSelectionTimeoutMS: 5000,
-    socketTimeoutMS: 45000,
-  };
+//   const options = {
+//     maxPoolSize: 10,
+//     serverSelectionTimeoutMS: 5000,
+//     socketTimeoutMS: 45000,
+//   };
 
-  if (!cached.promise) {
-    cached.promise = mongoose
-      .connect(MONGODB_URI, options)
-      .then((mongoose) => {
-        console.log("✅ MongoDB Connected!");
-        return mongoose;
-      })
-      .catch((err) => {
-        console.error("❌ MongoDB Connection Error:", err);
-        throw err;
-      });
-  }
+//   if (!cached.promise) {
+//     cached.promise = mongoose
+//       .connect(MONGODB_URI, options)
+//       .then((mongoose) => {
+//         console.log("✅ MongoDB Connected!");
+//         return mongoose;
+//       })
+//       .catch((err) => {
+//         console.error("❌ MongoDB Connection Error:", err);
+//         throw err;
+//       });
+//   }
 
-  cached.conn = await cached.promise;
-  return cached.conn;
-}
+//   cached.conn = await cached.promise;
+//   return cached.conn;
+// }
 
-export default connectDB;
+// export default connectDB;
